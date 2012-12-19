4ffed22d7cab45bce3bad8c56481b289bb4305fd
bbrodriges <bender@rodriges.org>
2012-12-19 12:16:31 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,86 @@
-pholcidae
+pholcidae - Tiny python web crawler
 =========
 
-Tiny python web crawler
+Pholcidae
+------------
+
+Pholcidae, commonly known as cellar spiders, are a spider family in the suborder Araneomorphae.
+
+About
+------------
+
+Pholcidae is a tiny Python module allows you to fast and easy write your own crawl spider.
+
+Dependencies
+------------
+
+* python >= 2.6.x
+
+Basic example
+------------
+
+``` python
+from pholcidae import Pholcidae
+
+class MySpider(Pholcidae):
+
+	settings = {'domain': 'www.test.com', 'start_page': '/sitemap/'}
+
+	def crawl(self, data):
+    	print(data.url)
+
+spider = MySpider()
+spider.start()
+```
+
+Allowed settings
+------------
+
+Settings must be passed as dictionary directly to subclass of Pholcidae.
+Params you can use:
+
+**Required**
+
+* **domain** _string_ - defines domain which pages will be parsed. Defines without trailing slash.
+
+**Additional**
+
+* **start_page** _string_ - URL which will be used as entry point to parsed site. Default: `/`
+* **protocol** _string_ - defines protocol to be used by crawler. Default: `http://`
+* **stay_in_domain** _bool_ - defines ability of crawler to leave passed domain to crawl out-of-domain pages. Default: `True`
+* **valid_link** _string_ - regular expression string, which will be used to filter site URLs. Default: `(.*)`
+* **autostart** _bool_ - defines if crawler will be starter right after class initialization. Default: `False`
+* **cookies** _dict_ - a dictionary of string key-values which represents cookie name and cookie value to be passed with site URL request. Default: `{}`
+
+Example with all settings
+------------
+``` python
+from pholcidae import Pholcidae
+
+class MySpider(Pholcidae):
+
+	settings = {
+		'domain': 'www.test.com',
+		'start_page': '/mypage/',
+		'protocol': 'http://',
+		'stay_in_domain': False,
+		'valid_link': 'product-(.*).html',
+		'autostart': True,
+		'cookies': {
+			'session': 'KSnD5KtjKDTde2Q9WxVy4iaav7a2EK73V'
+		}
+	}
+
+	def crawl(self, data):
+		query = 'INSERT INTO crawled (url, status, body) VALUES("%s", %i, "%s")' % (data.url, data.status, data.body)
+    	database.execute(query)
+
+spider = MySpider()
+```
+
+In this example our crawler will parse `http://www.test.com` starting with `http://www.test.com/mypage/`. It will parse only URLs with will be like `product-xxxxx.html` (example: `product-2357451.html`). Also crawler will parse any link with does not belong to `http://www.test.com`, but they must apply `valid_link` rule. Cookie header `Cookie: session=KSnD5KtjKDTde2Q9WxVy4iaav7a2EK73V` will be attached to every page request. Crawler will be started right after `spider = MySpider()`, without calling `spider.start()`.
+
+License
+------------
+
+This code uses no license. You can use it wherever and whatever you want.
---
+++ pholcidae.py
@@ -0,0 +1,266 @@
+import re
+import urlparse
+import urllib2
+
+
+class Pholcidae:
+
+    """" Pholcidae is a small and fast web crawler. """
+
+    def __init__(self):
+
+        """
+            @return void
+
+            Creates Pholcidae instance and updates default settings dict.
+        """
+
+        # default local urllib2 opener
+        self._opener = urllib2.build_opener()
+        # creating new lists of unparsed, alerady parsed and invalid URLs
+        self._unparsed_urls = set()
+        self._parsed_urls = set()
+        # extending settings with given values
+        self._extend_settings()
+        # compiling regular expressions
+        self._compile_regexs()
+        # autostart crawler if settings allows
+        if self._settings.autostart:
+            self.start()
+
+    def crawl(self, response):
+
+        """
+            @type response AttrDict
+            @return void
+
+            Dummy method which can be overrided by inheriting Pholcidae class.
+            Use it to get html page and parse it as you want to.
+        """
+
+        pass
+
+    def start(self):
+
+        """
+            @return void
+
+            Simple crawler start trigger.
+        """
+
+        self._get_page()
+
+    ############################################################################
+    # PRIVATE METHODS                                                          #
+    ############################################################################
+
+    ############################ INIT METHODS ##################################
+
+    def _extend_settings(self):
+
+        """
+            @return void
+
+            Extends default settings with given settings.
+        """
+
+        # creating settings object
+        self._settings = AttrDict()
+
+        # filling up default settings
+        # do we need to follow HTTP redirects?
+        self._settings.follow_redirects = True
+        # what page links do we need to parse?
+        self._settings.valid_link = '(.*)'
+        # what is an entry point for crawler??
+        self._settings.start_page = '/'
+        # which domain should we parse?
+        self._settings.domain = ''
+        # should we ignor pages outside of the given domain?
+        self._settings.stay_in_domain = True
+        # which protocol do we need to use?
+        self._settings.protocol = 'http://'
+        # autostart crawler right after initialization?
+        self._settings.autostart = False
+        # cookies to be added to each request
+        self._settings.cookies = {}
+
+        # updating settings with given values
+        self._settings.update(self.settings)
+        # compiling cookies for better usage
+        self._compile_cookies()
+
+        # adding start point into unparsed list
+        start_url = '%s%s%s' % (self._settings.protocol, self._settings.domain,
+                                self._settings.start_page)
+        self._unparsed_urls.add(start_url)
+
+    def _compile_regexs(self):
+
+        """
+            @return void
+
+            Compiles regular expressions for further use.
+        """
+
+        # regexs container
+        self._regex = AttrDict()
+        # compiling regexs
+        flags = re.I | re.S # setting common flags
+        # collects all links across given page
+        self._regex.href_links = re.compile(r'<a\s(.*?)href="(.*?)"(.*?)>',
+                                            flags=flags)
+        self._regex.valid_link = re.compile(self._settings.valid_link,
+                                            flags=flags)
+        self._regex.split_header = re.compile(r'^(.*?):(.*)$', flags=flags)
+
+    def _compile_cookies(self):
+
+        """
+            @return void
+
+            Compiles given dict of cookies to string.
+        """
+
+        compiled = []
+        for name, value in self._settings.cookies.iteritems():
+            compiled.append('%s=%s' % (name, value))
+        self._settings.cookies = ','.join(compiled)
+        self._opener.addheaders.append(('Cookie', self._settings.cookies))
+
+    ########################## CRAWLING METHODS ################################
+
+    def _get_page(self):
+
+        """
+            @return bool
+
+            Fetches page by URL.
+        """
+
+        if not self._unparsed_urls:
+            # exiting out from recursion
+            return True
+
+        # getting link to get
+        url = self._unparsed_urls.pop()
+        # moving url from unparsed to parsed list
+        self._parsed_urls.add(url)
+
+        # fetching page
+        page = self._fetch_url(url)
+        if page.status == 200:
+            # collecting links from page
+            self._get_page_links(page.body, page.url)
+            # sending raw HTML to crawl function
+            self.crawl(page)
+
+        # trying next URL
+        self._get_page()
+
+    def _get_page_links(self, raw_html, url):
+
+        """
+            @type raw_html str
+            @type url str
+            @return void
+
+            Parses out all links from crawled web page.
+        """
+
+        links_groups = self._regex.href_links.findall(raw_html)
+        links = [group[1] for group in links_groups]
+        for link in links:
+            # comparing link with given pattern
+            if self._is_valid_link(link):
+                # getting link parts
+                link_info = urlparse.urlparse(link)
+                # if link not relative
+                if link_info.scheme or link_info.netloc:
+                    # if stay_in_domain enabled and link outside of domain scope
+                    if self._settings.stay_in_domain:
+                        if self._settings.domain not in link:
+                            continue # throwing out invalid link
+                else:
+                    # converting relative link into absolute
+                    link = urlparse.urljoin(url, link)
+                # if link was not previously parsed
+                if link not in self._parsed_urls:
+                    self._unparsed_urls.add(link)
+
+    def _is_valid_link(self, link):
+
+        """
+            @type link str
+            @return bool
+
+            Compares link with given regex to decide if we need to parse that
+            page.
+        """
+
+        if link and '#' not in link:
+            matches = self._regex.valid_link.search(link)
+            if matches:
+                return True
+        return False
+
+    ######################### URL FETCHING METHODS #############################
+
+    def _fetch_url(self, url):
+
+        """
+            @type url str
+            @return AttrDict
+
+            Fetches given URL and returns data from it.
+        """
+
+        # empty page container
+        page = AttrDict()
+
+        try:
+            # getting response from given URL
+            resp = self._opener.open(url)
+            page.body = resp.read()
+            page.url = resp.geturl()
+            page.headers = self._parse_headers(resp.info().headers)
+            page.status = resp.getcode()
+        except urllib2.HTTPError as error:
+            page.body = error.read()
+            page.status = error.code
+            page.url = url
+
+        return page
+
+    def _parse_headers(self, raw_headers):
+
+        """
+            @type raw_headers list
+            @return AttrDict
+
+            Parses headers returned by page.
+        """
+
+        # empty headers container
+        headers = AttrDict()
+
+        for header in raw_headers:
+            # removing extra characters
+            header = self._regex.split_header.search(header)
+            key, value = header.group(0), header.group(1).strip()
+            headers.__setattr__(key, value)
+        return headers
+
+
+class AttrDict(dict):
+
+    """ A dict that allows for object-like property access syntax. """
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, key, value):
+        self.update({key:value})
