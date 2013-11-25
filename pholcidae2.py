2d68a875671c7d7f123c92fe9e4c7d2610743da1
bbrodriges <bender@rodriges.org>
2013-11-25 22:29:08 +0400

    Cleaning

--- pholcidae2.py
+++ pholcidae2.py
@@ -1,10 +1,5 @@
 # -*- coding: UTF-8 -*-
 
-from urlparse import urlparse
-from threading import Thread
-import httplib, sys
-from Queue import Queue
-
 
 class Pholcidae2:
 
@@ -18,37 +13,7 @@ class Pholcidae2:
             Creates Pholcidae instance and updates default settings dict.
         """
 
-        concurrent = 200
-
-        self.q = Queue(concurrent*2)
-        for i in range(concurrent):
-            t = Thread(target=self.do_work)
-            t.daemon = True
-            t.start()
-
-        for url in ['http://google.com', 'http://ya.ru', 'http://auto.ru', 'http://rodriges.org', 'http://avito.ru', 'http://twitter.com']:
-            self.q.put(url.strip())
-        self.q.join()
-
-    def do_work(self):
-        while True:
-            url = self.q.get()
-            status, url = self.get_status(url)
-            self.do_something_with_result(status, url)
-            self.q.task_done()
-
-    def get_status(self, ourl):
-        try:
-            url = urlparse(ourl)
-            conn = httplib.HTTPConnection(url.netloc)
-            conn.request("HEAD", url.path)
-            res = conn.getresponse()
-            return res.status, ourl
-        except:
-            return "error", ourl
-
-    def do_something_with_result(self, status, url):
-        print status, url
+        pass
 
 
 sprider = Pholcidae2()
