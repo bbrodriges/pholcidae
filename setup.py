276abb7c652c9fa67e9e7d016c5e4eb9ae685134
bbrodriges <bender.rodriges@gmail.com>
2016-05-05 16:20:55 +0300

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
-from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
 
 import pholcidae2
 
 setup(
     name='pholcidae2',
-    version=pholcidae2.__version__
+    version=pholcidae2.__version__,
+    packages=find_packages(os.path.dirname(os.path.abspath(__file__)))
 )
